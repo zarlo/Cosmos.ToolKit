c9218db7278d733390e5e5428cee347017e587bd
kaleb <zarlo5899@gmail.com>
2016-08-09 17:23:36 +1000

    removed unused

--- Cosmos.ToolKit.Test.Kernel/Kernel.cs
+++ Cosmos.ToolKit.Test.Kernel/Kernel.cs
@@ -18,12 +18,13 @@ namespace Cosmos.ToolKit.Test.Kernel
             
         }
         int i = 0;
-        string text = @"Powered by the C# Open Source \n Managed Operating System.";
+        
         protected override void Run()
         {
             GFX.DrawText(50, 50, i.ToString());            
             GFX.DrawText(0, 20, "abcdefghijklnmopqrstuvwxyz1234567890\n<>.,\"';:[]#//!");
-            GFX.Font.renderString(0, 35, text);
+            GFX.Font.renderString(0, 5, "Powered by the C# Open Source ");
+            GFX.Font.renderString(0, 10, "Managed Operating System.");
             i++;
             //GFX.Font.stdColour = i;
             GFX.Clear();
